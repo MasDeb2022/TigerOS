120985e6b70468888e0f632b0216c7f65e79f037
Arseny <Bricky2021@WIN-G0IF1JEQGMU>
2024-12-29 12:15:38 +0300

    Добавьте файлы проекта.

---
+++ Kernel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace TigerOS
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("TigerOS v0.1-alpha");
+            Console.WriteLine("Type the text to get it echoed back.\n");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("tigeros/echo> ");
+            var input = Console.ReadLine();
+            Console.Write("RECEIVED INPUT: ");
+            Console.WriteLine(input + "\n");
+        }
+    }
+}
