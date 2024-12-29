302c33cf19a4296b5d70c0cc1dd80426a5fc1735
MasDeb2022 <114352293+MasDeb2022@users.noreply.github.com>
2024-12-29 13:04:16 +0300

    Update Kernel.cs

--- Kernel.cs
+++ Kernel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Sys = Cosmos.System;
@@ -7,11 +7,18 @@ namespace TigerOS
 {
     public class Kernel : Sys.Kernel
     {
+        private const ConsoleColor darkGreen = ConsoleColor.DarkGreen;
+        private const ConsoleColor red = ConsoleColor.Red;
+        private const ConsoleColor white = ConsoleColor.White;
 
         protected override void BeforeRun()
         {
             Console.Clear();
-            Console.WriteLine("TigerOS v0.1-alpha");
+            Console.Write("Welcome to ");
+            Console.ForegroundColor = darkGreen;
+            Console.Write("TigerOS v0.1-alpha");
+            Console.ForegroundColor = white;
+            Console.WriteLine("!");
             Console.WriteLine("Type the text to get it echoed back.\n");
         }
 
